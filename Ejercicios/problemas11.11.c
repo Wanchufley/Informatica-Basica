bcaf1b65d31b38cdc320b8918c8b2d98ec577691
Dixan Rivas <alu0101000763@ull.edu.es>
2016-12-19 08:26:13 +0000

    Pendiente de terminar eliminar duplicados del array

--- Ejercicios/problemas11.11.c
+++ Ejercicios/problemas11.11.c
@@ -1,12 +1,41 @@
 #include <stdio.h>
 
+int Numeros[20];
+int temp;
+
+void ordenar (int pos, int a[pos]) {
+	for (int i=0;i<pos-1;i++) {
+		for (int j=0;j<pos-1;j++) {
+			if (a[j] > a[j+1]) {
+				printf ("Actual: %d Siguiente: %d\n", a[j], a[j+1]);
+				temp = a[j+1];
+				a[j+1] = a[j];
+				a[j] = temp;
+			}
+		}
+	}
+}
+void imprimir (int pos, int a[pos]) {
+	for (int i=0;i<pos;i++){ 
+		printf ("%d\t", Numeros[i]); 
+	}
+	printf ("\n");
+}
+void duplicados (int pos, int a[pos]) {
+	for (int i=0;i<pos;i++) {
+		for (int j=0;j<pos;j++) {
+			if (a[j] < a[j+1]) {
+				temp = a[j];
+				a[j] = a[j+1];	
+			}				
+		}
+	}
+}
 int main() {
 	FILE *arch1;
 	FILE *arch2;
 	int pos = 0;
 	int buff1,buff2;
-	int Numeros[20];
-	int temp;
 		
 	for (int i=0;i<20;i++){ Numeros[i] = 0;	}
 	arch1 = fopen ("archivo1.txt", "r");
@@ -29,19 +58,8 @@ int main() {
 	} else {
 		printf ("No se pudo abrir uno de los archivos\n");	
 	}
-	for (int i=0;i<pos;i++) {
-		for (int j=0;j<pos-1;j++) {
-			if (Numeros[j] > Numeros[j+1]) {
-				temp = Numeros[j+1];
-				Numeros[j+1] = Numeros[j];
-				Numeros[j] = temp;
-			}
-		}
-	}	
-	for (int i=0;i<pos;i++){ 
-		if (Numeros[i] != Numeros[i-1]) {
-			printf ("%d\t", Numeros[i]); 
-		}
-	}
-	printf ("\n");
+	ordenar (pos,Numeros);
+	duplicados (pos,Numeros);
+	imprimir (pos,Numeros);
 }
+
