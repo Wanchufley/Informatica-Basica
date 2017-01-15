7d1838c69c496e69b5977c74d83c76755861fce1
Dixan Rivas <alu0101000763@ull.edu.es>
2017-01-15 16:22:04 +0000

    Version Final

--- Ejercicios/problemas11.22.c
+++ Ejercicios/problemas11.22.c
@@ -1,38 +1,27 @@
 #include <stdio.h>
-#include <time.h>
-#include <stdlib.h>
 
 int size,sizer = 0;
 int nums[100];
 int res[100];
 
-void numAleatorios() {
-	FILE *fp;
-	int buff;
-	
-	fp = fopen ("numeros.txt", "w+");
-	for (int i=0;i<100;i++) {
-		fprintf (fp, "%d\n", rand() % 101);
-	}
-	fclose(fp);
-}
-void leerNumeros () {
-	FILE *fp;
-	int buff;
-	fp = fopen ("numeros.txt", "r");
-	if (fp != NULL) {
-		while (!feof(fp)) {
-			fscanf (fp, "%d\n", &buff);			
+void inicio () {
+	FILE *numeros;
+	FILE *final;
+	int buff,tmp;
+	int count = 0;
+
+	numeros = fopen ("numeros.txt", "r");
+	final = fopen ("final.txt", "w+");
+	if (numeros != NULL) {
+		while (!feof(numeros)) {
+			fscanf (numeros, "%d\n", &buff);			
 			nums[size] = buff;
 			size++;	
 		}
 	} else {
 		printf ("No se puede abrir el archivo\n");
 	}
-	fclose(fp);
-}
-void ordenar () {
-	int tmp;
+	fclose(numeros);
 	for (int i=0; i<size-1; i++) {
 		for (int j=0;j<size-1;j++) {
 			if (nums[j] > nums[j+1]){
@@ -42,33 +31,20 @@ void ordenar () {
 			}		
 		}
 	}
-}
-void duplicados () {
-	int count = 0;
 	for (int i=0; i<size-1; i++) {
-		if (nums[i] == nums[i+1] && nums[i] != res[sizer-1]) {
+		if (nums[i] == nums[i+1] && nums[i] != res[sizer-1]) 
 			count++;	
-		}	
 		if (count > 0) {
 			res[sizer] = nums[i];
 			sizer++;
 			count = 0;
 		}	
 	}
-}
-void guardar () {
-	FILE *fp;
-	fp = fopen ("final.txt", "w+");
 	for (int i=0;i<sizer;i++) {
-		fprintf (fp,"%d\n", res[i]);
+		fprintf (final,"%d\n", res[i]);
 	}
-	fclose(fp);
+	fclose(final);
 }
 int main () {
-	srand(time(NULL));
-	numAleatorios();
-	leerNumeros();
-	ordenar();
-	duplicados();
-	guardar();
+	inicio();
 }
