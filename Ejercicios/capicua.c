a1ca0e6d36191325c1ec3a57b73a315d7158f1dd
Dixan Rivas <alu0101000763@ull.edu.es>
2017-01-16 01:03:47 +0000

    Limpiar Codigo
    No evaluar toda la cadena de texto si al haber un elemento que no sea igual ya sé sabe no es capicua

--- Ejercicios/capicua.c
+++ Ejercicios/capicua.c
@@ -2,17 +2,18 @@
 
 int main () {
 	char palabra[20];
-	int i,lon,capicua = 0;
+	int i,j,capicua = 0;
 	printf ("Introduzca una palabra\n");
 	scanf ("%s", palabra);
 	getchar();
-
-	while (palabra[i] != '\0') {
-		lon++;
-		i++;
+	while (palabra[i] != '\0') { i++; } // Determinar cantidad de caracteres 	
+	for (j;j<i;j++) {
+		if (palabra[j] != palabra[i-j-1]) { 
+			printf ("La palabra %s no es capicua\n", palabra);
+			break; // Si los caracteres no son iguales ya sabemos no es capicua y salimos del bucle
+		} else {
+			capicua++; // Aumentar por cada caracter igual que encuentre
+		} 
 	}	
-	for (int i=0;i<lon;i++) {
-		palabra[i] == palabra[lon-i-1] ? (capicua++) : (0);
-	}	
-	capicua == lon ?  printf ("La palabra %s es capicua\n", palabra) : printf ("La palabra %s no es capicua\n", palabra);
+	capicua == i ? printf ("La palabra %s es capicua\n", palabra) : (0); // Si todos los caracteres son iguales el numero de capicua tiene que ser igual a la cantidad de caracteres de la cadena que es i
 }
