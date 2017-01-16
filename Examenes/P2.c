1fbffbfe19e5d37c0ef4cb96a95a1f59eb0f9e1f
Dixan Rivas <alu0101000763@ull.edu.es>
2017-01-16 14:05:23 +0000

    Comentarios de los errores

--- Examenes/P2.c
+++ Examenes/P2.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
-typedef struct {
+typedef struct { // No puse struct puse proveedor
 	int codigo;
 	char nombre[20];
 	char direccion[50];
 } proveedor_t;
 
-typedef struct { 
+typedef struct { // No puse struct puse datos 
 	char descripcion;
 	int cantidad;
 	float precio;
