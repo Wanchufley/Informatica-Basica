9bdaeb878987b7417bc27f3c9d84247df4668bc3
Dixan Rivas <alu0101000763@ull.edu.es>
2017-01-22 20:30:13 +0000

    Refactorizar y Arreglo de errores
    Arreglado omision de numeros en archivo final y añadida funcion para intercambiar valores

--- Ejercicios/problemas11.22.c
+++ Ejercicios/problemas11.22.c
@@ -1,50 +1,43 @@
 #include <stdio.h>
 
-int size,sizer = 0;
+int size = 0,sizer = 0;
 int nums[100];
 int res[100];
+
+void swap (int *num1, int *num2) {
+	int temp = *num2;
+	*num2 = *num1;
+	*num1 = temp;	
+}
 
 void inicio () {
 	FILE *numeros;
 	FILE *final;
 	int buff,tmp;
-	int count = 0;
 
 	numeros = fopen ("numeros.txt", "r");
 	final = fopen ("final.txt", "w+");
 	if (numeros != NULL) {
 		while (!feof(numeros)) {
 			fscanf (numeros, "%d\n", &buff);			
-			nums[size] = buff;
-			size++;	
+			nums[size++] = buff;
 		}
 	} else {
 		printf ("No se puede abrir el archivo\n");
-	}
-	fclose(numeros);
-	for (int i=0; i<size-1; i++) {
-		for (int j=0;j<size-1;j++) {
-			if (nums[j] > nums[j+1]){
-				tmp = nums[j+1];
-				nums[j+1] = nums[j];
-				nums[j] = tmp;
-			}		
+	} fclose(numeros);
+	for (int i=0; i<size; i++) {
+		for (int j=0;j<size;j++) {
+			nums[i] < nums[j] ? swap (&nums[i], &nums[j]) : (0); 
 		}
 	}
-	for (int i=0; i<size-1; i++) {
-		if (nums[i] == nums[i+1] && nums[i] != res[sizer-1]) 
-			count++;	
-		if (count > 0) {
-			res[sizer] = nums[i];
-			sizer++;
-			count = 0;
-		}	
+	for (int i=0; i<size; i++) {
+		nums[i] != nums[i+1] && nums[i] != res[sizer] ? res[sizer++] = nums[i] : (0);
 	}
 	for (int i=0;i<sizer;i++) {
 		fprintf (final,"%d\n", res[i]);
-	}
-	fclose(final);
+	} fclose(final);
 }
+
 int main () {
 	inicio();
 }
