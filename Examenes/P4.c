befb3e3bf764cad55e5f3f0aca98a9ed11627e15
Dixan Rivas <alu0101000763@ull.edu.es>
2017-01-28 17:51:16 +0000

    Cambiar Condicionales, Reducir Codigo

--- Examenes/P4.c
+++ Examenes/P4.c
@@ -1,26 +1,14 @@
 #include <stdio.h>
 
 int contar (char p[]) {
-	int i = 0;
-	int palabras = 0;
-	// int i, palabras = 0; Esto fue lo que puse y compila pero luego da error
-	while (*(p+i) != '\0') {
-		if (*(p+i) == ' ' && *(p+(i-1)) != ' ') {
-			palabras++;
-		}
-	i++; // Creo puse el incremento fuera del while
+	int i = 0,palabras = 0;
+	while (p[i] != '\0') {
+		p[i] == ' ' && p[i-1] != ' ' ? palabras++ : 0 ;
+		i++;
 	}
-	/* p[0] == '\0' ? return 0 : (0);
-	i != 0 && p[i] == '\0' ? return palabras+1 : (0);
-	Lo puse asi y esta mal porque ? es un operador no un control de flujo la forma correcta es el if de abajo */
-	if (*p == '\0') {
-		return 0;
-	}
-	if (i != 0 && *(p+i) == '\0') {
-		return palabras+1;
-	}
+	return (p[0] == '\0' ? 0 : palabras+1);
 }
-// Esto no habia que hacerlo en el examen
+
 int main () {
 	char cadena[] = {"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc elit."};
 	int palabras = contar (cadena);
