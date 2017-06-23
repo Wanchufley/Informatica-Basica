215b58348e331f6a6325d439df2421fc1ed6df5b
Wanchufley <iglesiasperezcarlos@gmail.com>
2017-06-24 00:27:53 +0100

    Update problemas11.10.c
    mira ver si eso es correcto desde q puedas

--- Ejercicios/problemas11.10.c
+++ Ejercicios/problemas11.10.c
@@ -1,2 +1,22 @@
 // Escribir una funcion para concatenar dos archivos del mismo tipo.
-// TODO
+#include <stdio.h>
+
+int main(){
+  FILE *file1;
+  FILE *file2;
+  FILE *finalfile;
+  char a;
+  char b;
+
+  file1 = fopen ("impar.txt", "r");
+  file2 = fopen ("par.txt", "r");
+  finalfile = fopen ("parimpar.txt", "w+");
+  while (!feof(file1)){
+    a = fgetc(file1);
+    fputc(a, finalfile);
+  }fclose(file1);
+  while (!feof(file2)){
+    b = fgetc(file2);
+    fputc(b, finalfile);
+  }fclose(file2);
+}
